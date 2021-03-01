fb35ae0d149dded98717133a788e6bd5defbdda6
DanielChin <76833985+DanielAungChin@users.noreply.github.com>
2021-03-01 20:30:31 +0900

    first example

---
+++ LINQtoSQL_EX2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Linq.Mapping;
+using System.Data.Linq;
+
+namespace LINQtoSQL_EX2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [Table(Name="Student_table")]
+        public class Contact
+        {
+            [Column]
+            public int student_id;
+            [Column]
+            public string student_Name;
+            [Column]
+            public string Gender;
+            [Column]
+            public string Course;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string ConnectiionString= @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user\Documents\Visual Studio 2019\Projects\exercise\LINQtoSQL_EX2\Student.mdf;Integrated Security=True;Connect Timeout=30";
+            try
+            {
+                DataContext db = new DataContext(ConnectiionString);//create DataContext
+                Table < Contact >contacts = db.GetTable<Contact>();//create table
+                var contactDetail = from c in contacts where c.Gender == "W" orderby c.student_Name select c;//LINQ to SQL
+                foreach(var c in contactDetail)
+                {
+                    richTextBox1.AppendText(c.student_Name);
+                    richTextBox1.AppendText("\t");
+                    richTextBox1.AppendText(c.Course);
+                    richTextBox1.AppendText("\n");
+                }
+            }
+            catch(Exception ec)
+            {
+                MessageBox.Show(ec.ToString());
+            }
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿--create table Student_table(student_Id int not null primary key,
+--student_Name nvarchar(100),Gender nvarchar(100),Course nvarchar(120));
+insert into Student_table values(1,N'山田','M','C101'),
+(2,N'西尾','W','C256'),
+(3,N'菅根','W','C154'),
+(4,N'神戸','W','C106'),
+(5,N'長田','M','C259'),
+(6,N'大阪','G','C147');
+select * from Student_table;
+
+
